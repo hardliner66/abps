aa0698ddafde9079a25555354232cb0f83a73f16
hardliner66 <hardliner66@gmail.com>
2024-03-26 19:50:42 +0000

    MIRROR

---
+++ src/actor.zig
@@ -0,0 +1,193 @@
+const std = @import("std");
+const mem = std.mem;
+const erase = @import("erase.zig");
+const lfq = @import("lfqueue.zig");
+
+const Allocator = mem.Allocator;
+
+pub const Behavior = *const fn (self: *Actor, sys: *System, state: *Any, from: ActorRef, msg: *Any) anyerror!void;
+
+fn toOpaque(behavior: Behavior) *anyopaque {
+    return @constCast(
+        @ptrCast(@alignCast(behavior)),
+    );
+}
+
+fn fromOpaque(behavior: *anyopaque) Behavior {
+    return @constCast(
+        @ptrCast(@alignCast(behavior)),
+    );
+}
+
+pub const Actor = struct {
+    ref: ActorRef,
+    behavior: *anyopaque,
+    last_behavior: ?*anyopaque,
+    state: Any,
+
+    pub fn init(ref: ActorRef, state: Any, behavior: Behavior) Actor {
+        return .{
+            .behavior = toOpaque(behavior),
+            .state = state,
+            .ref = ref,
+            .last_behavior = null,
+        };
+    }
+
+    pub fn deinit(self: *Actor) void {
+        self.state.deinit();
+    }
+
+    pub fn become(self: *Actor, behavior: Behavior) void {
+        self.last_behavior = self.behavior;
+        self.behavior = toOpaque(behavior);
+    }
+
+    pub fn unbecome(self: *Actor) void {
+        if (self.last_behavior) |b| {
+            self.behavior = b;
+            self.last_behavior = null;
+        }
+    }
+};
+
+pub const ActorRef = struct {
+    name: []const u8,
+};
+
+pub const DeadLetter = struct {
+    msg: Message,
+};
+
+pub const Message = struct {
+    to: ActorRef,
+    from: ActorRef,
+    msg: Any,
+};
+
+pub const System = struct {
+    actors: std.StringHashMap(Actor),
+    queue: lfq.LfQueue(*Message),
+    running: bool,
+    allocator: Allocator,
+
+    pub fn new(alloc: Allocator) @This() {
+        return .{
+            .allocator = alloc,
+            .actors = std.StringHashMap(Actor).init(alloc),
+            .queue = lfq.LfQueue(*Message).init(alloc),
+            .running = true,
+        };
+    }
+
+    pub fn destroy(self: *System) void {
+        var it = self.actors.valueIterator();
+        while (it.next()) |value_ptr| {
+            value_ptr.deinit();
+        }
+        self.actors.deinit();
+
+        while (self.queue.pop()) |msg| {
+            msg.msg.deinit();
+        }
+        self.queue.deinit();
+    }
+
+    pub fn spawn(self: *System, T: type, state: T, behavior: Behavior) !ActorRef {
+        const actor = Actor.init(
+            .{ .name = "test" },
+            try any(self.allocator, T, state),
+            behavior,
+        );
+        try self.actors.put("test", actor);
+        return .{ .name = "test" };
+    }
+
+    pub fn send(self: *System, from: ActorRef, to: ActorRef, comptime T: type, msg: T) !void {
+        const m = try self.allocator.create(Message);
+        m.from = from;
+        m.to = to;
+        m.msg = try any(
+            self.allocator,
+            T,
+            msg,
+        );
+        try self.queue.push(m);
+    }
+
+    pub fn stop(self: *System) void {
+        self.running = false;
+    }
+
+    pub fn work(self: *System) !void {
+        while (self.running) {
+            if (self.queue.pop()) |msg| {
+                if (self.actors.getPtr(msg.from.name)) |actor| {
+                    const behavior: Behavior = fromOpaque(actor.behavior);
+                    try behavior(actor, self, &actor.state, msg.from, &msg.msg);
+                    if (!msg.msg.read) {
+                        msg.msg.debug(msg.msg.ptr);
+                    }
+                    msg.msg.deinit();
+                }
+            } else {
+                break;
+            }
+        }
+    }
+};
+
+pub const Any = struct {
+    ptr: erase.AnyPointer,
+    allocator: Allocator,
+    dealloc: *const fn (allocator: Allocator, ptr: erase.AnyPointer) void,
+    debug: *const fn (ptr: erase.AnyPointer) void,
+    read: bool,
+
+    pub fn makeDealloc(comptime T: type) (fn (allocator: Allocator, ptr: erase.AnyPointer) void) {
+        return struct {
+            fn dealloc(allocator: Allocator, ptr: erase.AnyPointer) void {
+                const p = ptr.cast(*T);
+                allocator.destroy(p);
+            }
+        }.dealloc;
+    }
+
+    pub fn makeDebug(comptime T: type) (fn (ptr: erase.AnyPointer) void) {
+        return struct {
+            fn debug(ptr: erase.AnyPointer) void {
+                const p = ptr.cast(*T);
+                std.debug.print("Could not deliver message: {any}\n", .{p.*});
+            }
+        }.debug;
+    }
+
+    pub fn init(allocator: Allocator, comptime T: type, v: T) !Any {
+        const value = try allocator.create(T);
+        value.* = v;
+        return Any{
+            .ptr = erase.AnyPointer.make(*T, value),
+            .read = false,
+            .allocator = allocator,
+            .dealloc = Any.makeDealloc(T),
+            .debug = Any.makeDebug(T),
+        };
+    }
+
+    pub fn matches(self: *Any, comptime T: type) ?T {
+        const ptr = self.ptr.tryCast(*T);
+        if (ptr) |p| {
+            self.read = true;
+            return p.*;
+        }
+        return null;
+    }
+
+    pub fn deinit(self: *const Any) void {
+        self.dealloc(self.allocator, self.ptr);
+    }
+};
+
+pub fn any(allocator: Allocator, comptime T: type, v: T) !Any {
+    return Any.init(allocator, T, v);
+}
---
+++ src/erase.zig
@@ -0,0 +1,145 @@
+const std = @import("std");
+const builtin = @import("builtin");
+
+pub const AnyPointer = SafePointer;
+
+/// A type-erased pointer. Will perform safety checks in safe modes, otherwise will invoke undefined behaviour.
+// pub const AnyPointer = if (std.debug.runtime_safety)
+// SafePointer
+// else
+//     UnsafePointer;
+
+/// A type-checking type-erased pointer. Can contain *any* pointer and can be converted back to the original one.
+pub const SafePointer = struct {
+    /// Pointer to a invalid value.
+    pub const null_pointer = SafePointer{ .address = 0, .type_id = @as(TypeId, @enumFromInt(0)) };
+
+    address: usize,
+    type_id: TypeId,
+
+    /// Creates a new type-erased pointer.
+    pub fn make(comptime T: type, ptr: T) SafePointer {
+        assertPointer(T);
+        return SafePointer{
+            .address = @intFromPtr(ptr),
+            .type_id = typeId(T),
+        };
+    }
+
+    /// Casts the type-erased pointer to the pointer type `T`. Will perform a safety check and panic, if the pointer isn't of type `T`.
+    /// Returns `T`.
+    pub fn cast(self: SafePointer, comptime T: type) T {
+        assertPointer(T);
+        if (typeId(T) != self.type_id) {
+            if (builtin.mode == .Debug) {
+                std.debug.panic("Type mismatch: Expected {s}, but got {s}!", .{ @typeName(T), self.type_id.name() });
+            } else {
+                std.debug.panic("Type mismatch: Expected {s}, but got <unknown>!", .{@typeName(T)});
+            }
+        }
+        return @as(T, @ptrFromInt(self.address));
+    }
+
+    /// Will try to cast the type-erased pointer to `T`. Does return `null` if the types don't match, otherwise will
+    /// return the pointer as `T`.
+    pub fn tryCast(self: SafePointer, comptime T: type) ?T {
+        assertPointer(T);
+        if (self.isNull())
+            return null;
+        return if (typeId(T) == self.type_id)
+            @as(T, @ptrFromInt(self.address))
+        else
+            null;
+    }
+
+    /// Returns true if the pointer is a null pointer
+    pub fn isNull(self: SafePointer) bool {
+        return self.address == 0;
+    }
+
+    /// Returns true if the address of both pointers is the same.
+    pub fn eql(self: SafePointer, other: SafePointer) bool {
+        return self.address == other.address;
+    }
+};
+
+/// A type-erased pointer. Can contain *any* pointer and can be converted back to the original one.
+pub const UnsafePointer = enum(usize) {
+    /// Pointer to a invalid value.
+    null_pointer,
+    _,
+
+    /// Creates a new type-erased pointer.
+    pub fn make(comptime T: type, ptr: T) UnsafePointer {
+        assertPointer(T);
+        return @as(UnsafePointer, @enumFromInt(@intFromPtr(ptr)));
+    }
+
+    /// Will return the type-erased pointer as `T`.
+    pub fn cast(self: UnsafePointer, comptime T: type) T {
+        assertPointer(T);
+        return @as(T, @ptrFromInt(@intFromEnum(self)));
+    }
+
+    /// Returns true if the pointer is a null pointer
+    pub fn isNull(self: UnsafePointer) bool {
+        return self == .null_pointer;
+    }
+
+    /// Returns true if the address of both pointers is the same.
+    pub fn eql(self: UnsafePointer, other: UnsafePointer) bool {
+        return self == other;
+    }
+};
+
+const TypeId = enum(usize) {
+    _,
+
+    pub fn name(self: TypeId) []const u8 {
+        if (builtin.mode == .Debug) {
+            return std.mem.sliceTo(@as([*:0]const u8, @ptrFromInt(@intFromEnum(self))), 0);
+        } else {
+            @compileError("Cannot use TypeId.name outside of Debug mode!");
+        }
+    }
+};
+
+fn assertPointer(comptime T: type) void {
+    comptime var ti: std.builtin.Type = @typeInfo(T);
+    if (ti == .Optional) {
+        ti = @typeInfo(ti.Optional.child);
+    }
+    if (ti != .Pointer)
+        @compileError("any-pointer only works with (optional) pointers to one or many.");
+    switch (ti.Pointer.size) {
+        .One, .Many, .C => {},
+        else => @compileError("any-pointer only works with (optional) pointers to one or many."),
+    }
+}
+
+fn typeId(comptime T: type) TypeId {
+    const Tag = if (builtin.mode == .Debug)
+        struct {
+            const str = @typeName(T);
+            var name: [str.len:0]u8 = str.*;
+        }
+    else
+        struct {
+            var name: u8 = 0;
+        };
+    return @as(TypeId, @enumFromInt(@intFromPtr(&Tag.name)));
+}
+
+test "basic pointer" {
+    var i: u32 = 0;
+
+    const erased = AnyPointer.make(*u32, &i);
+
+    const ptr = erased.cast(*u32);
+
+    try std.testing.expectEqual(@as(*u32, &i), ptr);
+
+    ptr.* = 42;
+
+    std.debug.assert(i == 42);
+}
